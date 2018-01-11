09f8523999a2cc5aef2064e762f1b7e23adb549b
xushengyuan <xushengyuan123@outlook.com>
2018-01-12 00:24:46 +0800

    first commit

---
+++ bangumis.py
@@ -0,0 +1,35 @@
+import urllib
+import json
+import os
+import libxmlrpc
+SettingsPath='.\\settings.json'
+SettingsFile=open(SettingsPath,"r+")
+SettingsJson=json.loads(SettingsFile)
+LibrariesPath=SettingsJson['libraries_settings']['libraries_path']
+
+def GetTagName(id):
+    from urllib import request,parse
+    IdJson={'_ids':id}
+    IdData=parse.urlencode(IdJson) 
+    RequestUrl='https://bangumi.moe/api/tag/fetch'
+    Headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+    Request=request.Request(url=RequestUrl,data=IdData.encode('utf-8'),headers=Headers)
+    ResponseData=urllib.request.urlopen(Request).read().decode('utf-8')
+    ResponseJson=json.load(ResponseData)
+    return 
+def DownloadBanguni(id):
+    
+def GetItemNOTInLibraries(id):
+    
+def GetBangumiListUseTags(Tags):
+    from urllib import request,parse
+    IdJson={'tag_id':Tags}
+    IdData=json.dumps(IdJson) 
+    RequestUrl='https://bangumi.moe/api/torrent/search'
+    Headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
+    Request=request.Request(url=RequestUrl,data=IdData.encode('utf-8'),headers=Headers)
+    ResponseData=urllib.request.urlopen(Request).read().decode('utf-8')
+    ResponseJson=json.loads(ResponseData)
+    BangumiIds=[x['_id'] for x in ResponseJson['torrents']]
+    return BangumiIds
+def LibrariesAddItem(BangumiId,ItemId):
---
+++ update.py
@@ -0,0 +1,14 @@
+import urllib
+import json
+import os
+SettingsPath='.\\settings.json'
+def GetBangumiName(id):
+    IdData={'_ids':id}
+    IdJson=json.dumps(IdData) 
+def ImportSettingns:
+    0.
+    SettingsFile=open(settings_path,"r+")
+    SettingsJson=json.load(SettingsFile.read())
+def main:
+    
+main()
